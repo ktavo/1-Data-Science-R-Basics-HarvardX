df585d109e5e4b98721e7f610aef2c9be94330aa
Gustavo Ríos Páez <ktavo@ktavo.ubuntu>
2020-08-30 16:47:52 -0300

    Adding first data revision for data set

--- murdersDataset.R
+++ murdersDataset.R
@@ -7,3 +7,28 @@ murders %>%
   ggplot(aes(population, total, label = abb, color = region)) +
   geom_label()
     
+
+
+#Upload Data for murders
+library(dslabs)
+data("murders")
+
+#class of murders
+class(murders)
+
+#Object structure
+str(murders)
+
+#variable (column/vector) of the data frame
+murders$population
+
+#Get all columns from DF
+names(murders)
+
+#Length of a vector
+population <- murders$population
+length(population)
+
+#Levels for a specific factor
+levels(murders$region)
+
